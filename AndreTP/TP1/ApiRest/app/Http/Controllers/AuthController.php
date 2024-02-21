14323e42553db2b5f2b2a4b7c6546087aa5bfde3
precieuxfassinou <precieuxfassinou@gmail.com>
2024-02-21 12:29:00 +0100

    AndreTP

---
+++ AndreTP/TP1/ApiRest/app/Http/Controllers/AuthController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class AuthController extends Controller
+{
+    public function register(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|email|unique:users',
+            'password' => 'required|string|min:6',
+        ]);
+
+        $user = User::create([
+            'name' => $request->name,
+            'email' => $request->email,
+            'password' => Hash::make($request->password),
+            'profile' => 'user',
+        ]);
+
+        return response()->json(['user' => $user]);
+    }
+}
---
+++ AndreTP/TP1/ApiRest/app/Models/User.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class User extends Authenticatable
+{
+    use HasApiTokens, Notifiable;
+
+    protected $fillable = [
+        'name', 'email', 'password', 'profile', 'is_admin',
+    ];
+
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+}
